04863fdb1f2765934086a484b29571583459753a
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2021-08-19 10:43:33 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+target=/tmp/dupa
+mkdir -p $target
+
 function symlink() {
-  ln -s $1 $HOME/$2
+  ln -s $1 $target/$2
 }
 
+# VIM
 symlink vim .vim
 symlink vimrc .vimrc
+vim +qall
+
+# Emacs
+which emacs && {
+  echo "TODO: install Doom Emacs and my config files"
+}
 
