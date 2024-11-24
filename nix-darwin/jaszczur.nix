5f888edc2e1f44de935bf77bed54d97060370df0
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-11-24 08:51:03 +0100

    configure bat

--- nix-darwin/jaszczur.nix
+++ nix-darwin/jaszczur.nix
@@ -32,6 +32,38 @@
     };
   };
 
+  programs.bat = {
+    enable = true;
+    themes = let
+      catppuccin_src = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "bat";
+        rev = "d2bbee4f7e7d5bac63c054e4d8eca57954b31471";
+        sha256 = "sha256-x1yqPCWuoBSx/cI94eA+AWwhiSA42cLNUOFJl7qjhmw=";
+      };
+    in {
+      catppuccin-latte = { src = catppuccin_src;
+        file = "themes/Catppuccin Latte.tmTheme";
+      };
+      catpuccin-frappe = {
+        src = catppuccin_src;
+        file = "themes/Catppuccin Frappe.tmTheme";
+      };
+      catpuccin-macchiato = {
+        src = catppuccin_src;
+        file = "themes/Catppuccin Macchiato.tmTheme";
+      };
+      catpuccin-mocha = {
+        src = catppuccin_src;
+        file = "themes/Catppuccin Mocha.tmTheme";
+      };
+    };
+
+    config = {
+      theme = "catpuccin-macchiato";
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "Piotr Jaszczyk";
@@ -57,9 +89,11 @@
     vimAlias = true;
   };
 
-  # programs.nushell = {
-  #   enable = true;
-  # };
+  programs.nushell = {
+    enable = true;
+    environmentVariables = {
+    };
+  };
 
   programs.starship = {
     enable = true;
