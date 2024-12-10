df1c20421a2354d9417e707dd3b00d9f63bdb693
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-10 17:04:29 +0100

    nvim: split config, update plugins, lock versions in git/nix

---
+++ nvim/lua/custom/plugins/autoformat.lua
@@ -0,0 +1,42 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  lazy = false,
+  keys = {
+    {
+      '<leader>bf',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[B]uffer [F]ormat',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      ocaml = { 'ocamlformat' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use a sub-list to tell conform to run *until* a formatter
+      -- is found.
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      nix = { 'alejandra' },
+    },
+  },
+}
