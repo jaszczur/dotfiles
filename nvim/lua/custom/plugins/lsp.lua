07e54ef0ed439e9a7eab47adabf3c1ade2dccf7c
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-10 18:43:11 +0100

    nvim: backport from upstream

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -203,7 +203,7 @@ return { -- LSP Configuration & Plugins
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
+      -- But for many setups, the LSP (`ts_ls`) will work just fine
       cssls = {},
       eslint = {},
       html = {},
@@ -256,7 +256,7 @@ return { -- LSP Configuration & Plugins
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          -- certain features of an LSP (for example, turning off formatting for ts_ls)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           nvim_lsp[server_name].setup(server)
         end,
