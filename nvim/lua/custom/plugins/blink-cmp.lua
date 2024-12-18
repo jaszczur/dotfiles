d4aad472a55fffc5e09ecf9e989165793129194c
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2024-12-18 17:00:54 +0100

    blink: keymap modification

--- nvim/lua/custom/plugins/blink-cmp.lua
+++ nvim/lua/custom/plugins/blink-cmp.lua
@@ -19,7 +19,10 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      ['<C-e>'] = { 'select_prev', 'fallback' },
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
