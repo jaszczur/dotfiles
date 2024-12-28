ced280e40a50f536928a77d48338aabb56cf460f
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-28 11:51:16 +0100

    wezterm: appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,12 +5,26 @@ local action = wezterm.action
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Macchiato'
+-- Apperance
+local scheme_for_appearance = function(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Macchiato'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font = wezterm.font 'IosevkaTerm Nerd Font'
 config.font_size = 16.0
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.window_background_opacity = 0.90
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
 
 -- Default shell
 config.default_prog = { '/etc/profiles/per-user/jaszczur/bin/zsh', '-c', '/run/current-system/sw/bin/nu' }
@@ -80,7 +94,7 @@ local on_workspace_updated = function(window, workspace)
   local gui_win = window:gui_window()
   local base_path = string.gsub(workspace, '(.*[/\\])(.*)', '%2')
   gui_win:set_right_status(wezterm.format {
-    { Foreground = { Color = 'green' } },
+    { Foreground = { Color = '#a6da95' } },
     { Text = '  ' .. base_path .. '  ' },
   })
 end
