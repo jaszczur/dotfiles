a2c404fba05d063382fd4e3dee0f2c929c2285ed
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2025-01-24 10:15:47 +0100

    rust leptos lsp config

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -223,7 +223,18 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          procMacro = {
+            ignored = {
+              --  https://book.leptos.dev/getting_started/leptos_dx.html
+              leptos_macro = {
+                -- optional: --
+                -- "component",
+                'server',
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
