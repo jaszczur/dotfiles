94c3d7bc33b319774a120a647cdcd2c976966c6e
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2025-01-24 10:15:28 +0100

    rust tools & ollama

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}:
 with pkgs; [
   alejandra # formatter for nixfiles
-  bacon
-  cargo-binstall
   clojure
   clang-tools # for clang-format used by openscad-lsp
   coursier # for Scala
@@ -13,9 +11,16 @@ with pkgs; [
   # lorri
   nixd
   nushell
+  ollama
   openscad-lsp
   tenv
   ripgrep
-  rustup
   xh
+
+  #rust
+  bacon
+  cargo-binstall
+  leptosfmt
+  rustup
+  trunk
 ]
