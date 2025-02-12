14e23b9320ebdf0948c7449431687d1a6c30b5e9
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2025-02-12 07:52:05 +0100

    llm: enable copilot chat, add translation hooks

--- nvim/lua/custom/plugins/llm.lua
+++ nvim/lua/custom/plugins/llm.lua
@@ -1,3 +1,24 @@
+local function translate_fn(lang)
+  return function(gp, params)
+    local template = 'You are a Translator. Please translate the following text to'
+      .. lang
+      .. ':\n\n'
+      .. '```{{selection}}\n```\n\n'
+      .. '\n\nRespond exclusively with the snippet that should replace the selection above.'
+
+    local agent = gp.get_command_agent()
+
+    gp.Prompt(
+      params,
+      gp.Target.rewrite,
+      agent,
+      template,
+      nil, -- command will run directly without any prompting for user input
+      nil -- no predefined instructions (e.g. speech-to-text from Whisper)
+    )
+  end
+end
+
 return {
   {
     'robitx/gp.nvim',
@@ -14,6 +35,14 @@ return {
             secret = { 'nu', '-c', 'use authinfo.nu; authinfo password api.openai.com' },
           },
           googleai = { disable = true },
+          copilot = {
+            disable = false,
+            secret = {
+              'bash',
+              '-c',
+              "cat ~/.config/github-copilot/hosts.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+            },
+          },
           ollama = { disable = false },
         },
         agents = {
@@ -35,6 +64,10 @@ return {
         chat_shortcut_delete = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-c>d' },
         chat_shortcut_stop = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-c>s' },
         chat_shortcut_new = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-c>c' },
+        hooks = {
+          TranslateFi = translate_fn 'Finnish',
+          TranslateSv = translate_fn 'Swedish',
+        },
       }
       require('gp').setup(conf)
 
@@ -46,34 +79,9 @@ return {
           mode = { 'v' },
           nowait = true,
           remap = false,
-          { '<leader>oc', ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = 'AI chat buffer with selection' },
-          -- { '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", desc = 'Visual Append (after)' },
-          -- { '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", desc = 'Visual Prepend (before)' },
-          -- { '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", desc = 'Visual Chat New' },
-          -- { '<C-g>g', group = 'generate into new ..' },
-          -- { '<C-g>ge', ":<C-u>'<,'>GpEnew<cr>", desc = 'Visual GpEnew' },
-          -- { '<C-g>gn', ":<C-u>'<,'>GpNew<cr>", desc = 'Visual GpNew' },
-          -- { '<leader>oc', ":<C-u>'<,'>GpVnew<cr>", desc = 'Visual GpVnew' },
-          -- { '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", desc = 'Visual Popup' },
-          -- { '<C-g>gt', ":<C-u>'<,'>GpTabnew<cr>", desc = 'Visual GpTabnew' },
-          -- { '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", desc = 'Visual GpVnew' },
-          -- { '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", desc = 'Implement selection' },
-          -- { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
-          -- { '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", desc = 'Visual Chat Paste' },
-          -- { '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", desc = 'Visual Rewrite' },
-          -- { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
-          -- { '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", desc = 'Visual Toggle Chat' },
-          -- { '<C-g>w', group = 'Whisper' },
-          -- { '<C-g>wa', ":<C-u>'<,'>GpWhisperAppend<cr>", desc = 'Whisper Append' },
-          -- { '<C-g>wb', ":<C-u>'<,'>GpWhisperPrepend<cr>", desc = 'Whisper Prepend' },
-          -- { '<C-g>we', ":<C-u>'<,'>GpWhisperEnew<cr>", desc = 'Whisper Enew' },
-          -- { '<C-g>wn', ":<C-u>'<,'>GpWhisperNew<cr>", desc = 'Whisper New' },
-          -- { '<C-g>wp', ":<C-u>'<,'>GpWhisperPopup<cr>", desc = 'Whisper Popup' },
-          -- { '<C-g>wr', ":<C-u>'<,'>GpWhisperRewrite<cr>", desc = 'Whisper Rewrite' },
-          -- { '<C-g>wt', ":<C-u>'<,'>GpWhisperTabnew<cr>", desc = 'Whisper Tabnew' },
-          -- { '<C-g>wv', ":<C-u>'<,'>GpWhisperVnew<cr>", desc = 'Whisper Vnew' },
-          -- { '<C-g>ww', ":<C-u>'<,'>GpWhisper<cr>", desc = 'Whisper' },
-          -- { '<C-g>x', ":<C-u>'<,'>GpContext<cr>", desc = 'Visual GpContext' },
+          { '<leader>ocC', ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = 'New chat with selection' },
+          { '<leader>occ', ":<C-u>'<,'>GpChatToggle vsplit<cr>", desc = 'Toggle chat with selection' },
+          { '<leader>oci', ":<C-u>'<,'>GpImplement<cr>", desc = 'Implement selection' },
         },
 
         -- NORMAL mode mappings
@@ -81,33 +89,10 @@ return {
           mode = { 'n' },
           nowait = true,
           remap = false,
-          { '<leader>oc', '<cmd>GpVnew<cr>', desc = 'AI chat prompt' },
-          { '<leader>oC', '<cmd>GpChatNew vsplit<cr>', desc = 'AI chat buffer' },
-          -- { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
-          -- { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
-          -- { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
-          -- { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
-          -- { '<C-g>g', group = 'generate into new ..' },
-          -- { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
-          -- { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
-          -- { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
-          -- { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
-          -- { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
-          -- { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
-          -- { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
-          -- { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
-          -- { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
-          -- { '<C-g>w', group = 'Whisper' },
-          -- { '<C-g>wa', '<cmd>GpWhisperAppend<cr>', desc = 'Whisper Append (after)' },
-          -- { '<C-g>wb', '<cmd>GpWhisperPrepend<cr>', desc = 'Whisper Prepend (before)' },
-          -- { '<C-g>we', '<cmd>GpWhisperEnew<cr>', desc = 'Whisper Enew' },
-          -- { '<C-g>wn', '<cmd>GpWhisperNew<cr>', desc = 'Whisper New' },
-          -- { '<C-g>wp', '<cmd>GpWhisperPopup<cr>', desc = 'Whisper Popup' },
-          -- { '<C-g>wr', '<cmd>GpWhisperRewrite<cr>', desc = 'Whisper Inline Rewrite' },
-          -- { '<C-g>wt', '<cmd>GpWhisperTabnew<cr>', desc = 'Whisper Tabnew' },
-          -- { '<C-g>wv', '<cmd>GpWhisperVnew<cr>', desc = 'Whisper Vnew' },
-          -- { '<C-g>ww', '<cmd>GpWhisper<cr>', desc = 'Whisper' },
-          -- { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
+          { '<leader>ocC', '<cmd>GpChatNew vsplit<cr>', desc = 'New chat' },
+          { '<leader>occ', '<cmd>GpChatToggle vsplit<cr>', desc = 'Toggle last used chat' },
+          { '<leader>ocn', '<cmd>GpNextAgent<cr>', desc = 'Next agent' },
+          { '<leader>ocp', '<cmd>GpPopup<cr>', desc = 'Popup' },
         },
       }
     end,
